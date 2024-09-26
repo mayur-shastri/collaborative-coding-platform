ff6f2149042e7e781fb79b48c0d018c83455f1b4
Mau_2993 <shastrimayur2993@gmail.com>
2024-09-26 17:47:14 +0530

    vite.config.js proxy removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_BACKEND_URL
-          : 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.NODE_ENV === 'production' ? import.meta.env.VITE_BACKEND_URL
+  //         : 'http://localhost:3000',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     },
+  //   },
+  // },
   css: {
     postcss: './postcss.config.js',
   },
