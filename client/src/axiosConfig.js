1aab2e7af6c82ecc4d965543f185f35160efd0d6
Mau_2993 <shastrimayur2993@gmail.com>
2024-09-26 16:37:32 +0530

    Commit for deployment

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? 
-  'http://localhost:3000' : import.meta.env.BACKEND_URL,
+  'http://localhost:3000' : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
