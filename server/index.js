66b93db6f649e509902a46b77b31c580b5c7fcd6
Mau_2993 <shastrimayur2993@gmail.com>
2024-09-29 11:29:07 +0530

    Changes for production - 1

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const sessionConfig = {
         maxAge: 1000*60*60*24*7, // 1 week
         expires: Date.now() + 1000*60*60*24*7,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        // sameSite: 'none',
     },
     store: MongoStore.create({ mongoUrl: mongoUrl })
 }
