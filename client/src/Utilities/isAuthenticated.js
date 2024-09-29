ac9a029ce9765b4530873b06c6d86d8449a27854
Mau_2993 <shastrimayur2993@gmail.com>
2024-09-29 14:36:05 +0530

    Temporary fix

--- client/src/Utilities/isAuthenticated.js
+++ client/src/Utilities/isAuthenticated.js
@@ -1,17 +1,18 @@
 import { instance as configuredAxios } from '../axiosConfig';
 
 const isAuthenticated = async () => {
-    try {
-        const res = await configuredAxios.get('/userdata');
-        if (res.data.user) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch(err){
-        console.log(err.message);
-        return false;
-    }
+    // try {
+    //     const res = await configuredAxios.get('/userdata');
+    //     if (res.data.user) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // } catch(err){
+    //     console.log(err.message);
+    //     return false;
+    // }
+    return true; //temporary fix
 }
 
 export { isAuthenticated };
